1715f3fa4ac7ebec96c6c87a0d9fc35cf8cdd23b
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-09-24 23:00:25 -0700

    Can now view goals

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,5 +2,6 @@ class UserController < ApplicationController
   def index
   	@dailies = Daily.where(:user_id => current_user.id)
   	@daily = Daily.new
+  	@goals = Goal.where(:user_id => current_user.id)
   end
 end
