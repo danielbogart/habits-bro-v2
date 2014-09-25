6a6dbf51d9bc0deba19cdebf56e63f65b902144e
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-09-25 00:19:22 -0700

    Added 24 hour timeframe for streak

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,5 +6,11 @@ class UserController < ApplicationController
   	@goal = Goal.new
   	@wants = current_user.wants
   	@want = Want.new
+
+  	current_user.dailies.each do |d|
+  		if d.updated_at < 24.hours.ago
+  			d.update_attributes(:streak => 0)
+  		end
+  	end
   end
 end
