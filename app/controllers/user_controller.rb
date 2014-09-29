826db0eb30dcfc88d2da24c9be27f42056d371a2
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-09-29 10:16:43 -0700

    Switched back to 24 hours

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,7 +8,7 @@ class UserController < ApplicationController
   	@want = Want.new
 
   	current_user.dailies.each do |d|
-  		if d.updated_at < 1.minute.ago
+  		if d.updated_at < 24.hours.ago
   			d.update_attributes(:streak => 0)
         flash[:error] = "Streak ended"
   		end
